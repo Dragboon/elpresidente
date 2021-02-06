c73cd7a3d54f027b936e0559a1691693abbefe90
Dragboon <ye.steven@hotmail.fr>
2021-02-06 15:05:09 +0100

    Rework homepage code
    Change how the menu works

--- El Presidente/src/main/java/fr/esgi/config/HomepageConfig.java
+++ El Presidente/src/main/java/fr/esgi/config/HomepageConfig.java
@@ -3,6 +3,19 @@ package fr.esgi.config;
 import java.util.Map;
 
 public class HomepageConfig {
-    public Map<String, String> homepage;
-    public Map<String, Double[]> difficulty;
+    private Map<String, String> title;
+    private Map<Integer, String> homepage;
+    private Map<String, Double[]> difficulty;
+
+    public Map<Integer, String> getHomepage() {
+        return homepage;
+    }
+
+    public Map<String, Double[]> getDifficulty() {
+        return difficulty;
+    }
+
+    public Map<String, String> getTitle() {
+        return title;
+    }
 }
---
+++ El Presidente/src/main/java/fr/esgi/menu/Homepage.java
@@ -0,0 +1,63 @@
+package fr.esgi.menu;
+
+import com.google.gson.Gson;
+import fr.esgi.config.ErrorsHandler;
+import fr.esgi.config.HomepageConfig;
+import fr.esgi.exceptions.SeasonDisplayNotFound;
+import fr.esgi.exceptions.SeasonFileException;
+import fr.esgi.game.Play;
+import fr.esgi.readers.HomepageReader;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Homepage extends InputMenu {
+
+    public void printMenu(Scanner scanner) {
+
+        HomepageReader homepageReader = new HomepageReader();
+        HomepageConfig homepageConfig;
+        int choice;
+
+        try {
+            homepageConfig = homepageReader.getMenu();
+        } catch (IOException e) {
+            System.out.println("Le fichier du menu n'existe pas.");
+            return;
+        }
+
+        System.out.println("\n" + homepageConfig.getTitle().get("name") + "\n\n");
+
+        do {
+            choice = display(scanner, homepageConfig.getHomepage().values().toArray(new String[0]));
+            menu(scanner, choice);
+        } while (choice != 5);
+
+    }
+
+    public void menu(Scanner scanner, int choice) {
+
+        Play game = new Play();
+
+        try {
+            switch (choice) {
+                case 1 :
+                    game.startGame(scanner);
+                    break;
+                case 2 :
+                    // méthode difficulté
+                    break;
+                case 3 :
+                    // sandbox
+                    break;
+                case 4 :
+                    // leaderboard
+                    break;
+            }
+        } catch (SeasonFileException | SeasonDisplayNotFound e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
